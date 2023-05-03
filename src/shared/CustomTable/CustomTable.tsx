ac77a1dd88b14e587a85bd58b55fac7869f5ef17
josh <you@example.com>
2023-05-03 18:06:05 +0530

    remove unwanted imports and comments in custom table

--- src/shared/CustomTable/CustomTable.tsx
+++ src/shared/CustomTable/CustomTable.tsx
@@ -1,17 +1,4 @@
-import {
-  DataGrid,
-  FilterColumnsArgs,
-  GetColumnForNewFilterArgs,
-  GridColDef,
-  GridToolbar,
-  GridToolbarQuickFilter,
-} from "@mui/x-data-grid";
-import React, { useState } from "react";
-import { getAllConfigColumns } from "../../pages/GetAllConfigurations/constants";
-import { Box, Hidden } from "@mui/material";
-import useFetchAllConfigurations from "../../hooks/useFetchAllConfig";
-// import "./style.css";
-// import { Box } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 interface IProps {
   data: any;
@@ -19,61 +6,6 @@ interface IProps {
 }
 const CustomTable = (props: IProps) => {
   const { data, columnHeader } = props;
-
-  /*
-  function QuickSearchToolbar() {
-  return (
-  <Box
-  sx={{
-  p: 0.5,
-  pb: 0,
-  }}
-  >
-  <GridToolbarQuickFilter />
-  </Box>
-  );
-  }
-  */
-  /*
-  const filterColumns = ({
-    field,
-    columns,
-    currentFilters,
-  }: FilterColumnsArgs) => {
-    // remove already filtered fields from list of columns
-    const filteredFields = currentFilters?.map((item) => item.field);
-    return columns
-      .filter(
-        (colDef: any) =>
-          colDef.filterable &&
-          (colDef.field === field || !filteredFields.includes(colDef.field))
-      )
-      .map((column: any) => column.field);
-  };
-
-  const getColumnForNewFilter = ({
-    currentFilters,
-    columns,
-  }: GetColumnForNewFilterArgs) => {
-    const filteredFields = currentFilters?.map(({ field }) => field);
-    const columnForNewFilter = columns
-      .filter(
-        (colDef: any) =>
-          colDef.filterable && !filteredFields.includes(colDef.field)
-      )
-      .find((colDef: any) => colDef.filterOperators?.length);
-    return columnForNewFilter?.field ?? null;
-  }; 
-  */
-
-  // const [searchValue, setSearchValue] = useState<string>("");
-  // const [searchTrigger, setSearchTrigger] = useState<any>("");
-
-  // const { data, isLoading, isError } = useFetchAllConfigurations({
-  //   searchValue: searchTrigger,
-  // });
-
-  // console.log("sssss", data);
 
   return (
     <div style={{ height: "520px", width: "auto", marginRight: "20px" }}>
@@ -83,17 +15,6 @@ const CustomTable = (props: IProps) => {
         disableRowSelectionOnClick //Used to Remove statement: whenever we select rows it shows selected rows statement on UI.
         rows={data}
         columns={columnHeader}
-        // slots={{ toolbar: GridToolbar }}
-        // slotProps={{
-        // filterPanel: {
-        // filterFormProps: {
-        // filterColumns,
-        // },
-        // getColumnForNewFilter,
-        // },
-        // }}
-        // disableColumnSelector // Used to disable column selector.
-        // disableDensitySelector // Used to disable density selector.
         sx={{
           ".MuiDataGrid-iconButtonContainer": {
             visibility: "visible",
@@ -102,23 +23,6 @@ const CustomTable = (props: IProps) => {
             opacity: "inherit !important",
           },
         }}
-        // showCellVerticalBorder
-        // showColumnVerticalBorder
-
-        /*
-  slotProps={{
-  filterPanel: {
-  filterFormProps: {
-  // filterColumns,
-  },
-  // getColumnForNewFilter,
-  },
-  }}
-  slots={{ toolbar: QuickSearchToolbar }} // =>Used for searching on UI using material UI library
-  */
-
-        // pageSize={1}
-        // rowsPerPageOptions={[2]}
       />
     </div>
   );
