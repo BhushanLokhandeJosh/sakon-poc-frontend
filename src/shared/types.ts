c0f894598246076f1d05302ab56125c187555d31
BhushanLokhandeJosh <bhushan.lokhande@joshsoftware.com>
2023-05-10 15:30:18 +0530

    Conflicts solved

---
+++ src/shared/types.ts
@@ -0,0 +1,11 @@
+import { FormikProps } from "formik";
+
+export enum MAX_WIDTH {
+    XS = "xs",
+    SM = "sm",
+    MD = "md",
+    LG = "lg",
+    XL = "xl"
+}
+
+export interface IFormikProps<T> extends FormikProps<T> {}
