a966c13fcc3615af9d92b3ac4b8c9768e9129050
BhushanLokhandeJosh <bhushan.lokhande@joshsoftware.com>
2023-05-19 11:09:50 +0530

    UsePopover Hook Which handles popover added

---
+++ src/shared/CustomHooks/usePopover.ts
@@ -0,0 +1,23 @@
+import React from "react";
+
+//This Hook is design to handle popover throught application.
+const usePopover = () => {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
+
+  return { anchorEl, open, id, handleClick, handleClose };
+};
+
+export default usePopover;
