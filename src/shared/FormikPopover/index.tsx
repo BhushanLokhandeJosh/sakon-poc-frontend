6fe4bbda9e59c1f7f627c542f37ec89fce4f074f
BhushanLokhandeJosh <bhushan.lokhande@joshsoftware.com>
2023-05-23 15:52:20 +0530

    All PR Comments resolved...

--- src/shared/FormikPopover/index.tsx
+++ src/shared/FormikPopover/index.tsx
@@ -1,47 +1,36 @@
 import { Popover, Typography, Button } from "@mui/material";
 
 import { IFormikProps } from "../types";
+import usePopover from "../CustomHooks/usePopover";
 
 import "./styles/styles.css";
 
 import FormikContainer from "../FormikModalComponent/component/FormikContainer";
 
 export interface IFormikModalProp {
-  id: string;
-  isOpen: boolean;
-  anchorEl: HTMLButtonElement | null;
-  handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
-  handleClose: () => void;
-  popoverButtonClassName: string;
   popoverButtonTitle: string;
   popoverButtonIcon?: any;
-  popoverClassName?: string;
   popoverTitle: string;
   submitButtonLabel?: string;
-  onSubmit: (values: any) => void;
+  onSubmit: (values: any, handleClose: Function) => void;
   getFormBody: (formik: any) => JSX.Element;
   initialValues: any;
   //filter Box may not have validationSchema.
   validationSchema?: any;
+  popoverClassName?: string;
   formClassName?: string;
   showResetButton: boolean;
   showCancelButton: boolean;
 }
 
-// TODO:We have to check this component with Column Popover component later,
+//TODO:We have to check this component with Column Popover component later,
 // In order to avoid redundancy and enhancing functionality of custom shared
-// popover component.
+//popover component.
 
 const FormikPopoverComponent = (props: IFormikModalProp) => {
   const {
-    id,
-    isOpen,
-    anchorEl,
-    handleClick,
-    handleClose,
     popoverButtonTitle,
     popoverButtonIcon,
-    popoverButtonClassName,
     submitButtonLabel,
     popoverTitle,
     getFormBody,
@@ -54,52 +43,54 @@ const FormikPopoverComponent = (props: IFormikModalProp) => {
     showCancelButton,
   } = props;
 
+  const { anchorEl, id, open, handleClick, handleClose } = usePopover();
+
   const handleReset = (formik: IFormikProps<any>) => {
     formik.resetForm();
   };
 
   return (
     <div className={popoverClassName}>
-      <div className={popoverButtonClassName}>
-        <Button aria-describedby={id} variant="contained" onClick={handleClick}>
-          {popoverButtonTitle}
-          {popoverButtonIcon}
-        </Button>
-      </div>
+      <Button aria-describedby={id} variant="contained" onClick={handleClick}>
+        {popoverButtonTitle}
+        {popoverButtonIcon}
+      </Button>
 
-      <Popover
-        id={id}
-        open={isOpen}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "center",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "center",
-        }}
-      >
-        <Typography
-          variant="h5"
-          sx={{ margin: "1% 0% 1% 1%", fontWeight: "bold" }}
+      {open && (
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "center",
+          }}
         >
-          {popoverTitle}
-        </Typography>
-        <FormikContainer
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmitHandler={onSubmit}
-          formClassName={formClassName}
-          toggle={handleClose}
-          submitButtonLabel={submitButtonLabel}
-          showResetButton={showResetButton}
-          showCancelButton={showCancelButton}
-          handleReset={handleReset}
-          getFormikForm={(formik: IFormikProps<any>) => getFormBody(formik)}
-        />
-      </Popover>
+          <Typography
+            variant="h5"
+            sx={{ margin: "1% 0% 1% 1%", fontWeight: "bold" }}
+          >
+            {popoverTitle}
+          </Typography>
+          <FormikContainer
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmitHandler={(values) => onSubmit(values, handleClose)}
+            formClassName={formClassName}
+            toggle={handleClose}
+            submitButtonLabel={submitButtonLabel}
+            showResetButton={showResetButton}
+            showCancelButton={showCancelButton}
+            handleReset={handleReset}
+            getFormikForm={(formik: IFormikProps<any>) => getFormBody(formik)}
+          />
+        </Popover>
+      )}
     </div>
   );
 };
@@ -108,7 +99,6 @@ FormikPopoverComponent.defaultProps = {
   showResetButton: false,
   showCancelButton: true,
   popoverButtonTitle: "Open Popover",
-  popoverButtonClassName: "popover-button-style",
   popoverTitle: "",
 };
 
