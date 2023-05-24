daf5646fc6f3e7c8aed61c239712be2557963652
BhushanLokhandeJosh <bhushan.lokhande@joshsoftware.com>
2023-05-24 18:29:57 +0530

    Enum types added for input component

--- src/shared/FormComponents/Input.tsx
+++ src/shared/FormComponents/Input.tsx
@@ -1,32 +1,38 @@
-import { ReactElement } from "react";
+import { ChangeEvent, ReactElement } from "react";
 import { Field, ErrorMessage } from "formik";
 import { InputLabel } from "@mui/material";
 
 import FormError from "./FormError";
 
 import "./styles/styles.css";
+import { INPUT_TYPE } from "../types";
 
 interface InputElementProps {
   label?: string;
   name: string;
-  type?:
-    | "text"
-    | "date"
-    | "time"
-    | "textarea"
-    | "select"
-    | "email"
-    | "password"
-    | "number"
-    | "file"
-    | "radio";
+  type: INPUT_TYPE;
   className?: string;
   placeholder?: string;
   value: string | Blob;
+  onChange?: Function;
+  formikSetFieldValue?: any;
 }
 
 const Input = (props: InputElementProps): ReactElement => {
-  const { label, name, className, type, placeholder, value } = props;
+  const {
+    label,
+    name,
+    className,
+    type,
+    placeholder,
+    value,
+    formikSetFieldValue,
+  } = props;
+
+  const handleFileChange = (fileNameAttribute: string, event: any) => {
+    formikSetFieldValue(fileNameAttribute, event.currentTarget.files[0]);
+  };
+
   return (
     <>
       <InputLabel htmlFor={name} sx={{ color: "black" }}>
@@ -34,14 +40,27 @@ const Input = (props: InputElementProps): ReactElement => {
       </InputLabel>
 
       <div>
-        <Field
-          id={name}
-          name={name}
-          className={className}
-          type={type}
-          placeholder={placeholder}
-          value={value}
-        />
+        {type !== INPUT_TYPE.FILE ? (
+          <Field
+            id={name}
+            value={value}
+            name={name}
+            className={className}
+            type={type}
+            placeholder={placeholder}
+          />
+        ) : (
+          <input
+            type="file"
+            id="template"
+            name="template"
+            className="input-file-field"
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              handleFileChange(name, event)
+            }
+          />
+        )}
+
         <ErrorMessage name={name} component={FormError} />
       </div>
     </>
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,11 +1,24 @@
 import { FormikProps } from "formik";
 
 export enum MAX_WIDTH {
-    XS = "xs",
-    SM = "sm",
-    MD = "md",
-    LG = "lg",
-    XL = "xl"
+  XS = "xs",
+  SM = "sm",
+  MD = "md",
+  LG = "lg",
+  XL = "xl",
+}
+
+export enum INPUT_TYPE {
+  TEXT = "text",
+  DATE = "date",
+  TIME = "time",
+  TEXTAREA = "textarea",
+  SELECT = "select",
+  EMAIL = "email",
+  PASSWORD = "password",
+  NUMBER = "number",
+  FILE = "file",
+  RADIO = "radio",
 }
 
 export interface IFormikProps<T> extends FormikProps<T> {}
