c88f264575352cd394cd3dbc33c25dfeae1500d7
josh <you@example.com>
2023-05-24 11:21:22 +0530

    delete types.ts file and added type formik component

--- src/shared/FormikModalComponent/component/index.tsx
+++ src/shared/FormikModalComponent/component/index.tsx
@@ -10,7 +10,7 @@ export interface IFormikModalProp {
   isOpen: boolean;
   toggleModal: () => void;
   modalTitle: string;
-  getFormBody: any;
+  getFormBody: (formik: any) => JSX.Element;
   submitButtonLabel?: string;
   initialValues: any;
   validationSchema: any;
