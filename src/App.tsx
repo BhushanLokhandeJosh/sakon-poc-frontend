132f9bc61d83b57ce19047ddda9c8c6bf3d58f4e
josh <you@example.com>
2023-05-29 23:33:06 +0530

    remoed unwanted code

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Home from "./pages/Home";
 function App() {
   return (
     <Router>
-      <div className="App"></div>
       <Routes>
         <Route path="/" element={<Home />}></Route>
       </Routes>
