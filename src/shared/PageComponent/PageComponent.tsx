9258c537f23076996c111705fbdae7b9b7609a9b
josh <you@example.com>
2023-06-14 12:30:44 +0530

    added action button

--- src/shared/PageComponent/PageComponent.tsx
+++ src/shared/PageComponent/PageComponent.tsx
@@ -9,51 +9,6 @@ interface IProps {
   pageHeadingClassName: string;
   pageTitleClassName: string;
   pageButtonClassName: string;
-}
-
-const PageComponent = (props: IProps) => {
-  const {
-    pageTitle,
-    buttonLabel,
-    pageBody,
-    pageHeadingClassName,
-    pageTitleClassName,
-    pageButtonClassName,
-  } = props;
-  return (
-    <div>
-      <div className={pageHeadingClassName}>
-        <h1 className={pageTitleClassName}>{pageTitle}</h1>
-        {buttonLabel && (
-          <Button variant="contained" className={pageButtonClassName}>
-            {buttonLabel}
-          </Button>
-        )}
-      </div>
-      {pageBody}
-    </div>
-  );
-};
-
-PageComponent.defaultProps = {
-  pageHeadingClassName: "page-heading",
-  pageTitleClassName: "page-title",
-  pageButtonClassName: "page-button",
-};
-
-export default PageComponent;
-
-import { Button } from "@mui/material";
-import { ReactNode } from "react";
-
-import "./style/styles.css";
-interface IProps {
-  pageTitle: string;
-  buttonLabel?: string;
-  pageBody: ReactNode;
-  pageHeadingClassName: string;
-  pageTitleClassName: string;
-  pageButtonClassName: string;
   handleButtonAction: () => void;
 }
 
