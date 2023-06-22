7c666100445f906c1c41386f28ce01d4eb3ab2c1
Harshal <you@example.com>
2023-06-22 15:13:39 +0530

    done

--- src/pages/Dashboard/component/DashboardComponent.tsx
+++ src/pages/Dashboard/component/DashboardComponent.tsx
@@ -1,6 +1,9 @@
 import CustomPieChart from "../../../shared/Charts/CustomPieChart";
 import { useGetOrganizations } from "../config-hooks";
 import BarGraph from "../../../shared/Charts/CustomLineGraph";
+import { Formik, Form } from "formik";
+import { Button, Grid, InputLabel } from "@mui/material";
+import Select from "../../../shared/FormComponents/Select";
 const DashboardComponent = () => {
   const data1 = [
     { name: "Success", value: 400 },
@@ -26,12 +29,56 @@ const DashboardComponent = () => {
     console.log("selected value", e.target.value);
   };
 
+  const onSubmit = (values: any) => {
+    console.log(values);
+  };
+
+  const selectOptions: { label: string; value: string }[] = [];
+
+  organizationNames?.map((item: any) => {
+    const obj = {
+      label: item.name,
+      value: item.name,
+    };
+    selectOptions.push(obj);
+    console.log(selectOptions);
+  });
+
   return (
     <>
-      {/* <Formik initialValues={initialValue} onSubmit={onSubmit} onCh>
-        <Select name="organisation" label="organisation" options={OPTIONS} />
-      </Formik> */}
-      <label>Organization: </label>
+      <Formik initialValues={{ select: "" }} onSubmit={onSubmit}>
+        <Form>
+          <Grid
+            item
+            xs={5}
+            sx={{ display: "flex", marginTop: "2rem", alignItems: "baseline" }}
+          >
+            <Grid xs={3}>
+              <InputLabel sx={{ color: "black" }}>Organization</InputLabel>
+            </Grid>
+            <Grid xs={6}>
+              <Select
+                name="select"
+                placeholder="Select Configuration"
+                options={selectOptions}
+                className="configuration-style"
+                menuPlacement="bottom"
+                maxMenuHeight={100}
+                isMulti={false}
+              />
+            </Grid>
+          </Grid>
+
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ textTransform: "capitalize" }}
+          >
+            Submit
+          </Button>
+        </Form>
+      </Formik>
+      {/* <label>Organization: </label>
       <select
         name="organization"
         id="organization"
@@ -40,7 +87,7 @@ const DashboardComponent = () => {
         {organizationNames?.map((organization: any) => (
           <option value={organization.name}>{organization.name}</option>
         ))}
-      </select>
+      </select> */}
 
       <div style={{ display: "flex" }}>
         {/* <span
